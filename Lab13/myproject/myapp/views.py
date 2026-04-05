5e92b21a5494ece1d09321ea27088d22c559a5d9
Mussagorn <masakorn10@gmail.com>
2026-04-05 13:51:08 +0700

    Add files via upload

---
+++ Lab13/myproject/myapp/views.py
@@ -0,0 +1,48 @@
+from urllib import request
+from django.shortcuts import render , redirect 
+from django.http import HttpResponse
+from myapp.models import Person
+from  django.db.models import Q
+
+# Create your views here.
+def index(request):
+       search_query = request.GET.get('search', '')
+       
+       if search_query:
+           all_Person = Person.objects.filter(Q(name__icontains=search_query) | Q(age__icontains=search_query))
+       else:
+           all_Person = Person.objects.all()
+       
+       return render(request, "index.html" , {
+           "all_Person": all_Person,
+           "search_query": search_query
+       })
+    
+
+
+def form(request, id=None):
+    person = None
+    if id:
+        person = Person.objects.get(id=id)
+    
+    if request.method == "POST":
+        name = request.POST.get("name")
+        age = request.POST.get("age")
+        
+        if id:
+            person.name = name
+            person.age = age
+            person.save()
+        else:
+            person = Person.objects.create(name=name , age=age)
+        return redirect("/")
+    else:
+        return render(request, "form.html", {"person": person})
+
+def about(request):
+      return render(request, "about.html")
+
+def delete(request, id):
+    person = Person.objects.get(id=id)
+    person.delete()
+    return redirect("/")
